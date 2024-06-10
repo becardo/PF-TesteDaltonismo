00d1f8ceec778444d0166cef7c5cd8ea513ab79b
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2024-06-10 18:23:52 -0300

    Update TesteDaltonismo.py

--- TesteDaltonismo.py
+++ TesteDaltonismo.py
@@ -4,13 +4,13 @@ from tkinter import *
 class TesteDaltonismo(ABC):
     def __init__(self):
         super().__init__()
-        self._janela_teste = Tk()
-        self._setup_janela()
+        self.janela_teste = Tk()
+        self.setup_janela()
         
-    def _setup_janela(self):
-        self._janela_teste.title("Teste de Daltonismo")
-        self._janela_teste.geometry("800x600")
-        self._janela_teste.configure(background= '#F0F8FF')
+    def setup_janela(self):
+        self.janela_teste.title("Teste de Daltonismo")
+        self.janela_teste.geometry("800x600")
+        self.janela_teste.configure(background= '#F0F8FF')
         
     @abstractmethod
     def iniciar_teste(self):
