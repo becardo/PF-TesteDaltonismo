3b5a9e150b6fcf2e579ef45309b0910526b2316c
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2024-06-13 10:15:22 -0300

    Atualização InterfaceDaltonismo.py
    Adição do método habilitar_botao

--- InterfaceDaltonismo.py
+++ InterfaceDaltonismo.py
@@ -1,6 +1,18 @@
 from abc import ABC, abstractmethod
 
 class TesteDaltonismo(ABC):
+    
     @abstractmethod
     def iniciar_teste(self):
         pass
+
+    def habilitar_botao(self, botao, estado):
+        """
+        Habilita ou desabilita um botão baseado no nome do botão e no estado desejado.
+        :param botao: Nome do botão a ser modificado ('bt_proxima' ou 'bt_gerar_pdf').
+        :param estado: Estado desejado ('normal' ou 'disabled').
+        """
+        if hasattr(self, botao):
+            getattr(self, botao).config(state=estado)
+        else:
+            raise ValueError(f"Botão desconhecido: {botao}")
