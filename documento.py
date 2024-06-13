93e6f4bd54e05dce23b699ec81ef76b864f16887
isaac02901 <128757935+isaac02901@users.noreply.github.com>
2024-06-13 12:35:59 -0300

    Update documento.py
    precisa interligar com ishihara

--- documento.py
+++ documento.py
@@ -1,54 +1,81 @@
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-import webbrowser 
+import pandas as pd
+import pdfkit
+import webbrowser
 from usuario import Usuario
 from tkinter import Tk
-#from Ishihara import Ishihara
+from ishihara import Ishihara
 
 class Documento(Usuario):
-    def __init__(self, usuario, respostas, placas):
+    def __init__(self, usuario: list, respostas: list, placas: list) -> None:
         self.usuario = usuario
         self.respostas = respostas
         self.placas = placas
         self.gerar_relatorio_pdf()
-        
-    def relatorio_usuario(self):
+
+    def relatorio_usuario(self) -> None:
         webbrowser.open("ResultadoTeste.pdf")
-        
-    def gerar_relatorio_pdf(self):
-        self.canvas = canvas.Canvas("ResultadoTeste.pdf", pagesize=letter)
-        
-        self.canvas.setFont("Helvetica-Bold", 24)
-        self.canvas.drawString(200, 790, 'Resultados')
 
-        # Aqui você precisa corrigir a obtenção dos valores do usuário
-        self.nome = self.usuario.nome_var.get()
-        self.sobrenome = self.usuario.sobrenome_var.get()
-        self.data_nasc = self.usuario.data_var.get()
-        self.tel = self.usuario.entry_tel.get()  # telefone não está como StringVar
-        self.rua = self.usuario.rua_var.get()
-        self.numero = self.usuario.numero_var.get()
-        self.cep = self.usuario.entry_cep.get()  # CEP não está como StringVar
-        self.bairro = self.usuario.bairro_var.get()
-        self.cidade = self.usuario.cidade_var.get()
+    def gerar_relatorio_pdf(self) -> None:
+        # Obtenção dos valores do usuário
+        self.nome = self.usuario[0].nome_var.get()
+        self.sobrenome = self.usuario[0].sobrenome_var.get()
+        self.data_nasc = self.usuario[0].data_var.get()
+        self.tel = self.usuario[0].entry_tel.get()  # telefone não está como StringVar
+        self.rua = self.usuario[0].rua_var.get()
+        self.numero = self.usuario[0].numero_var.get()
+        self.cep = self.usuario[0].entry_cep.get()  # CEP não está como StringVar
+        self.bairro = self.usuario[0].bairro_var.get()
+        self.cidade = self.usuario[0].cidade_var.get()
 
-        self.canvas.setFont("Helvetica-Bold", 24)
-        self.canvas.drawString(150, 790, self.nome + " " + self.sobrenome)
-        self.canvas.drawString(150, 770, self.data_nasc)
-        self.canvas.drawString(150, 750, self.tel)
-        self.canvas.drawString(150, 730, self.rua)
-        self.canvas.drawString(150, 710, self.numero)
-        self.canvas.drawString(150, 690, self.cep)
-        self.canvas.drawString(150, 670, self.bairro)
-        self.canvas.drawString(150, 650, self.cidade)
-        
-        self.canvas.showPage()
-        self.canvas.save()
+        '''
+        Criação do DataFrame com os dados do usuário
+        '''
+        dados_usuario = {
+            'Nome': [self.nome],
+            'Sobrenome': [self.sobrenome],
+            'Data de Nascimento': [self.data_nasc],
+            'Telefone': [self.tel],
+            'Rua': [self.rua],
+            'Número': [self.numero],
+            'CEP': [self.cep],
+            'Bairro': [self.bairro],
+            'Cidade': [self.cidade]
+        }
+        df_usuario = pd.DataFrame(dados_usuario)
+
+        '''
+        Criação do DataFrame com os resultados do teste
+        '''
+        dados_teste = []
+        for i, resposta in enumerate(self.respostas):
+            dados_teste.append({
+                'Placa': i + 1,
+                'Resposta': resposta,
+                'Esperado': self.placas[i]['expected']
+            })
+        df_teste = pd.DataFrame(dados_teste)
+
+        '''
+        Salvando os DataFrames como arquivos HTML
+        '''
+        df_usuario.to_html('usuario.html', index=False)
+        df_teste.to_html('teste.html', index=False)
+
+        '''
+        Gerando o arquivo PDF a partir do HTML
+        '''
+        with open('relatorio.html', 'w') as f:
+            f.write('<h1>Resultados do Teste de Daltonismo</h1>')
+            f.write('<h2>Dados do Usuário</h2>')
+            with open('usuario.html', 'r') as usuario_html:
+                f.write(usuario_html.read())
+            f.write('<h2>Resultados do Teste</h2>')
+            with open('teste.html', 'r') as teste_html:
+                f.write(teste_html.read())
+
+        '''
+        Convertendo HTML para PDF
+        '''
+        pdfkit.from_file('relatorio.html', 'ResultadoTeste.pdf')
+
         self.relatorio_usuario()
-
-
-# Cria uma instância de Usuario
-#usuario_teste = Usuario()
-
-# Chama a classe Ishihara e passa a instância de Usuario
-#Ishihara(usuario_teste)
