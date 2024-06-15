24a98018c40a14a4ab2581ecfbaa8539f4942aa8
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2024-06-15 13:53:17 -0300

    Última alteração em InterfaceDaltonismo.py
    Com essa última alteração, InterfaceDaltonismo.py trás a classe TesteDaltonismo, que possui métodos que serão herdados por usuario.py e ishihara.py.

--- InterfaceDaltonismo.py
+++ InterfaceDaltonismo.py
@@ -3,10 +3,10 @@ from abc import ABC, abstractmethod
 class TesteDaltonismo(ABC):
     
     @abstractmethod
-    def iniciar_teste(self):
+    def iniciar_teste(self) -> None:
         pass
 
-    def habilitar_botao(self, botao, estado):
+    def habilitar_botao(self, botao: str, estado: str) -> None:
         """
         Habilita ou desabilita um botão baseado no nome do botão e no estado desejado.
         :param botao: Nome do botão a ser modificado ('bt_proxima' ou 'bt_gerar_pdf').
